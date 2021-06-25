1e78b7e8bc846683ae019eaa022d4a6db8e99a11
Snowden <55126792+Snowdenv20@users.noreply.github.com>
2021-06-25 15:00:42 -0600

    Expresiones II

---
+++ ExpresionesII/main.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include<math.h>//Libreria con funciones matematicas
+
+using namespace std;
+//Ecribe la siguiente expresion en c++:
+/*
+    b
+ a+ -
+    c
+----------
+    e
+ d+ -
+    f
+
+      b
+   a+ -
+     c-d
+*/
+int main()
+{
+    /*float a,b,c,d,e,f,resultado=0;
+    cout<<"Ingrese el valor de a:";cin>>a;
+    cout<<"Ingrese el valor de b:";cin>>b;
+    cout<<"Ingrese el valor de c:";cin>>c;
+    cout<<"Ingrese el valor de d:";cin>>d;
+    cout<<"Ingrese el valor de e:";cin>>e;
+    cout<<"Ingrese el valor de f:";cin>>f;
+    resultado=(a+(b/c))/(d+(e/f));
+    cout<<"El Resultado es:"<<resultado<<endl;*/
+
+
+    //Ejemplo 2 : realizar un Swap de los valores de X y Y
+   /* int x=0,y=0,aux=0;
+    cout<<"Ingrese el valor de x:";cin>>x;
+    cout<<"Ingrese el valor de y:";cin>>y;
+*/
+    /*
+    x=5
+    y=10
+    */
+
+    //aux=5
+    //aux=x;
+    //x=10
+    //x=y;
+    //y=5
+    /*y=aux;
+    cout<<"El valor de x es:"<<x<<endl;
+    cout<<"El valor de y es:"<<y<<endl;*/
+//Ejercicio realizado por Frida:Escriba un programa que lea la nota final de cuatro alumnos y calcule la nota final media.
+
+
+/*
+La calificacion final de un estudiante es la media ponderada de tres notas:
+La nota de practicas que cuenta con un 30% del total
+La nota teorica que cuenta con un 60%
+La nota de participacion que cuenta con el 10% restante
+Escriba un programa que lea de la entrada estandar las tres notas del alumno y escriba en la salida estandar su nota
+final.
+*/
+/*
+double NotaPractica=0,NotaTeorica=0,NotaParticipacion=0,NotaFinal=0;
+cout<<"Ingrese la nota de practica:";cin>>NotaPractica;
+cout<<"Ingrese la nota de Teorica:";cin>>NotaTeorica;
+cout<<"Ingrese la nota de Participacion:";cin>>NotaParticipacion;
+
+NotaPractica=NotaPractica*0.30;
+NotaTeorica*=0.60;
+NotaParticipacion*=0.10;
+
+NotaFinal=NotaParticipacion+NotaPractica+NotaTeorica;
+cout<<"La nota Final del alumno es de:"<<NotaFinal<<endl;*/
+
+/*
+Operador        Corta               Ejemplo en c++      igual
+---------------------------------------------------------------
++                +=                     x+=y            x=x+y
+-                -=                     x-=y            x=x-y
+*                *=                     x*=y            x=x*y
+/                /=                     x/=y            x=x/y
+
+*/
+
+//Ejemplo 5: Realice el programa que calcule el valor que toma la siguiente funcion para unos valores dados de x e y
+/*
+double x,y,resultado=0;
+cout<<"Ingrese el valor de X:";cin>>x;
+cout<<"Ingrese el valor de Y:";cin>>y;
+
+resultado=(sqrt(x))/(pow(y,2)-1);
+cout<<"El valor resultante es de:"<<resultado<<endl;
+*/
+    return 0;
+}
