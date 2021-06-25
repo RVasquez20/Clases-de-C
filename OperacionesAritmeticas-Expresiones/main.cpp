b35c7075e2510bba8aa26bee109ea3275920f0a0
Snowden <55126792+Snowdenv20@users.noreply.github.com>
2021-06-25 10:41:46 -0600

    Operadores Aritmeticos y expresiones

---
+++ OperacionesAritmeticas-Expresiones/main.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /*
+    Escriba un programa que lea de la entrada estandar dos numeros y muestre en la salida estandar
+    su suma, resta,multiplicacion y division.
+    */
+
+    /*double NumeroUno=0,NumeroDos=0,Suma=0,Resta=0,Multiplicacion=0,Division=0;
+    cout<<"Ingrese el primer numero a operar:";cin>>NumeroUno;
+    cout<<"Ingrese el segundo numero a operar:";cin>>NumeroDos;
+
+    Suma=NumeroUno+NumeroDos;
+    Resta=NumeroUno-NumeroDos;
+    Multiplicacion=NumeroUno*NumeroDos;
+    Division=NumeroUno/NumeroDos;
+    //cout<<endl<<"La Suma es:"<<Suma<<endl;
+    cout<<"\nLa Suma es:"<<Suma<<endl;
+    cout<<"La Resta es:"<<Resta<<endl;
+    cout<<"La Multiplicacion es:"<<Multiplicacion<<endl;
+    cout<<"La Division es:"<<Division<<endl;*/
+
+
+    //Ejercicio resuelto por Frida:Escribe un programa que lea de la entrada estandar el precio de un
+    //producto y muestre en la salida estandar el precio del producto al aplicarle el iva
+
+    /*
+    Realice un programa que lea de la entrada estandar los siguientes datos de una persona:
+    1)Edad:Dato de tipo entero
+    2)Sexo:Dato de tipo caracter
+    3)Altura en metros:Dato de tipo double
+    Tras leer los datos el programa debe mostrarlos en la salida estandar
+    */
+
+/*
+    int Edad=0;
+    char Sexo[10];
+    double Altura=0.0;
+
+    cout<<"Digite su edad:";cin>>Edad;
+    cout<<"Digite su sexo:";cin>>Sexo;
+    cout<<"Digite su Altura:";cin>>Altura;
+
+    cout<<endl<<"Edad:"<<Edad<<endl;
+    cout<<"Sexo:"<<Sexo<<endl;
+    cout<<"Altura:"<<Altura<<endl;
+
+*/
+
+/*Ejecute el programa del ejercicio anterior con entradas erroneas y observe los resultados.
+por ejemplo, introdusca un dato de tipo caracter cuando se espera un dato de tipo entero*/
+ /*int Edad=0;
+    char Sexo[10];
+    double Altura=0.0;
+
+    cout<<"Digite su edad:";cin>>Edad;
+    cout<<"Digite su sexo:";cin>>Sexo;
+    cout<<"Digite su Altura:";cin>>Altura;
+
+    cout<<endl<<"Edad:"<<Edad<<endl;
+    cout<<"Sexo:"<<Sexo<<endl;
+    cout<<"Altura:"<<Altura<<endl;*/
+
+    /*Ejemplo 5:Realice un programa el cual pueda solucionar la siguiente expresion algebraica: a/b +1*/
+    /*float a=0.0,b=0.0,Resultado=0.0;
+
+    cout<<"Ingrese el valor de a:";cin>>a;
+    cout<<"Ingrese el valor de b:";cin>>b;
+    Resultado=(a/b)+1;
+    cout.precision(2);//metodo que indica cuantos numeros seran mostrados
+    cout<<endl<<"El Resultado es:"<<Resultado<<endl;*/
+      return 0;
+}
