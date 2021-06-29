f00e481e783e41d31b54d4d703152a27a3a8a160
Snowden <55126792+Snowdenv20@users.noreply.github.com>
2021-06-29 14:57:22 -0600

    update

--- Sentencia IF/main.cpp
+++ Sentencia IF/main.cpp
@@ -43,6 +43,8 @@ int main()
     instruscciones;
     }
     */
+
+
     /*Ejemplo 1: Realizar un programa en donde el usuario ingrese un numero
     y verifique si es el numero 10,de serlo mostrar un mensaje de que gano
     un premio!! */
@@ -52,6 +54,8 @@ int main()
     if(numero==10){
       cout<<"Felicidades Gano!!"<<endl;
     }*/
+
+
     /*Ejemplo 2: Con el programa anterior cambie la condicion para que todos
     los numeros excepto el 10 ganen un premio*/
     /*
@@ -61,6 +65,32 @@ int main()
       cout<<"Felicidades Gano!!"<<endl;
     }*/
 
+    //Ejercio 1 hecho por frida
+    /*
+    Realice un programa en el cual el usuario ingrese 3 numeros , los sume y si el resultado sobrepasa 50 ,
+    debera mostrar un mensaje en pantalla diciendo que Gano!!
+    */
+
+
+/*Ejemplo 4:Iniciar una variable de tipo bool en el valor que desee, posteriormente si
+la variable es true mostrar un mensaje en pantalla diciendo que es verdadera :D
+si es falsa mostrar un mensaje en pantalla diciendo que es falsa  :c*/
+
+bool variable=false;
+/*if(variable==true){
+  cout<<"Es Verdadera :D";
+}
+
+if(variable==false){
+    cout<<"Es Falsa :C";
+}*/
+if(variable){
+  cout<<"Es Verdadera :D";
+}
+
+if(!variable){
+    cout<<"Es Falsa :C";
+}
 
     return 0;
 }
